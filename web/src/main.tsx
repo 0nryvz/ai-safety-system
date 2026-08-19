f5e060c406eb4b4f6fa97c6a87f4f7a4e75ae8c6
Ahmet Demirbilek <ahmett5400@gmail.com>
2026-08-20 02:38:36 +0300

    feat(web): add realtime reconnect reconciliation

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,12 +3,17 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import ErrorBoundary from './app/ErrorBoundary.tsx'
-import { startRealtimeRuntime } from './core/realtime/realtimeRuntime.ts'
+import { loadRealtimeRecoverySnapshots } from './core/realtime/realtimeRecoveryLoader.ts'
+import {
+  setRealtimeRecoverySnapshotLoader,
+  startRealtimeRuntime,
+} from './core/realtime/realtimeRuntime.ts'
 import { bootstrapAuthSession } from './features/auth/authBootstrap.ts'
 import './index.css'
 
 async function startApp() {
   await bootstrapAuthSession()
+  setRealtimeRecoverySnapshotLoader(loadRealtimeRecoverySnapshots)
   startRealtimeRuntime()
 
   createRoot(document.getElementById('root')!).render(
